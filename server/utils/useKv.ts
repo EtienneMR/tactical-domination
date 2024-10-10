1f347b3319a178057104675a4f54b71b60f6079b
Etienne_MR <61475533+EtienneMR@users.noreply.github.com>
2024-10-10 16:10:22 +0200

    Try using dev dependency

--- server/utils/useKv.ts
+++ server/utils/useKv.ts
@@ -1,9 +1,7 @@
-import { type Kv } from "@deno/kv";
+import { openKv, type Kv } from "@deno/kv";
 
 export const useKv: () => Promise<Kv> = async () => {
-  const OpenKV = () => import("@deno/kv");
-  const { openKv } = await OpenKV();
   if (process.dev) {
     return openKv("kv.db");
-  } else return openKv();
+  } else return Deno.openKv();
 };
