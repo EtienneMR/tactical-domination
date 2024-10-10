c862e498e5a042812e45ddf249efbc11a7d5ca9d
Etienne_MR <61475533+EtienneMR@users.noreply.github.com>
2024-10-10 16:13:53 +0200

    Fix globalThis test

--- server/utils/useKv.ts
+++ server/utils/useKv.ts
@@ -3,5 +3,6 @@ import { openKv, type Kv } from "@deno/kv";
 export const useKv: () => Promise<Kv> = async () => {
   if (process.dev) {
     return openKv("kv.db");
+    // @ts-expect-error globalThis not defined
   } else return globalThis.Deno.openKv();
 };
