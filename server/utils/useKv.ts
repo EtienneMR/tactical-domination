a71c1155ce41656abb946c4121897d74e4660f5a
Etienne_MR <61475533+EtienneMR@users.noreply.github.com>
2024-10-10 20:02:21 +0200

    Fixed build error

--- server/utils/useKv.ts
+++ server/utils/useKv.ts
@@ -1,20 +1,7 @@
 import { type Kv } from "@deno/kv";
 
-let warned = false;
-
 export const useKv: () => Promise<Kv> = async () => {
-  let useDev = import.meta.dev;
-
-  // @ts-expect-error globalThis not defined
-  if (!useDev && !globalThis.Deno) {
-    if (!warned) {
-      console.warn("Deno not found, using local kv");
-      warned = true;
-    }
-    useDev = true;
-  }
-
-  if (useDev) {
+  if (import.meta.dev) {
     const { openKv } = await import("@deno/kv");
     return openKv("kv.db");
 
