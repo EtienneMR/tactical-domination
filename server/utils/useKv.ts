2362a6d166bc2429660f5bf680df534228c975d7
Etienne_MR <61475533+EtienneMR@users.noreply.github.com>
2024-10-10 16:12:19 +0200

    Try using globalThis

--- server/utils/useKv.ts
+++ server/utils/useKv.ts
@@ -3,5 +3,5 @@ import { openKv, type Kv } from "@deno/kv";
 export const useKv: () => Promise<Kv> = async () => {
   if (process.dev) {
     return openKv("kv.db");
-  } else return Deno.openKv();
+  } else return globalThis.Deno.openKv();
 };
