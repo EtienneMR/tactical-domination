e1170d893be7d66e21165e7c02fe47575b0e9659
Etienne_MR <61475533+EtienneMR@users.noreply.github.com>
2024-10-10 16:08:13 +0200

    Try inline importing

--- server/utils/useKv.ts
+++ server/utils/useKv.ts
@@ -1,6 +1,8 @@
-import { openKv, type Kv } from "@deno/kv";
+import { type Kv } from "@deno/kv";
 
 export const useKv: () => Promise<Kv> = async () => {
+  const OpenKV = () => import("@deno/kv");
+  const { openKv } = await OpenKV();
   if (process.dev) {
     return openKv("kv.db");
   } else return openKv();
