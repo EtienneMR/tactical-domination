5233cb2f65ea26598ff3138dce386fa194917626
Etienne_MR <61475533+EtienneMR@users.noreply.github.com>
2024-10-10 17:05:32 +0200

    Test

--- server/utils/useKv.ts
+++ server/utils/useKv.ts
@@ -1,7 +1,8 @@
-import { openKv, type Kv } from "@deno/kv";
+import { type Kv } from "@deno/kv";
 
 export const useKv: () => Promise<Kv> = async () => {
   if (process.dev) {
+    // @ts-expect-error
     return openKv("kv.db");
 
     // @ts-expect-error globalThis not defined
