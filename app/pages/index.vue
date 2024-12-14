f35fbb44480e1d6be3122e043b3b10abccba7a15
Etienne_MR <61475533+EtienneMR@users.noreply.github.com>
2024-12-14 17:03:37 +0100

    Added icons to maps

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,7 +4,6 @@ import { MAPS } from "~~/shared/consts";
 import usePlayerId from "~/game/utils/usePlayerId";
 
 async function createAndJoinGame(mapName: string) {
-  const nuxtApp = useNuxtApp()
   const gid = `${Math.floor(Math.random() * 1000000)}`;
   try {
     await $fetch("/api/setupgame", {
@@ -30,14 +29,28 @@ async function createAndJoinGame(mapName: string) {
 <template>
   <div class="flex-1 p-4">
     <h1>Créer une partie</h1>
-    <p v-for="map of MAPS">
-      <UButton
-        @click="createAndJoinGame(map.id)"
-        trailing
-        icon="i-heroicons-arrow-right-20-solid"
-      >
-        {{ map.name }} </UButton
-      ><UBadge class="ml-3">{{ map.label }}</UBadge>
-    </p>
+    <div class="maplist">
+      <div v-for="map of MAPS" class="flex items-center">
+        <UButton
+          @click="createAndJoinGame(map.id)"
+          trailing
+          icon="i-heroicons-arrow-right-20-solid"
+        >
+          <img :src="`/maps/${map.id}.png`" /><span>{{
+            map.name
+          }}</span> </UButton
+        ><UBadge class="ml-3">{{ map.label }}</UBadge>
+      </div>
+    </div>
   </div>
 </template>
+
+<style scoped>
+.maplist {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(min(100vw, 400px), 1fr));
+  grid-gap: 1em;
+  justify-items: stretch;
+  width: 100%;
+}
+</style>
