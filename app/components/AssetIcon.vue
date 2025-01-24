2f4875476069bc28c2b4bac164ed02e7dd644a2c
Etienne_MR <61475533+EtienneMR@users.noreply.github.com>
2025-01-24 18:30:49 +0100

    Fix missing icons in GameMechanics

--- app/components/AssetIcon.vue
+++ app/components/AssetIcon.vue
@@ -1,5 +1,7 @@
 <script setup lang="ts">
 import useSettings from "~/game/utils/useSettings.js"
+import manifest from "~~/public/assets/manifest.json"
+
 
 const settings = useSettings()
 
@@ -7,11 +9,16 @@ const props = defineProps<{
   asset: string,
   label: string
 }>()
+
+const assetPath = computed(() => {
+  return manifest.bundles.find(b => b.name == settings.activeBundle)?.assets.find(asset => asset.alias == `${settings.activeBundle}:${props.asset}`)?.src
+})
+
 </script>
 <template>
   <span class="font-bold">
     <ClientOnly fallback-tag="div" class="h-[1em] w-[1em]">
-      <img :src="`/assets/${settings.bundle}/${props.asset}`" class="inline h-[1em] w-[1em]" alt="">
+      <img :src="assetPath" class="inline h-[1em] w-[1em]" alt="">
     </ClientOnly> {{ props.label }}
   </span>
 </template>
--- app/components/tabs/GameMechanics.vue
+++ app/components/tabs/GameMechanics.vue
@@ -24,10 +24,10 @@
       <TabsProseParagraph title="Ressources">
         Il existe deux types de ressources :
         <ul>
-          <li>La <AssetIcon asset="resources/food.png" label="nourriture" />, obtenable sur les <AssetIcon asset="buildings/null_wheat.png" label="champs" /> et les <AssetIcon asset="buildings/null_pasture.png" label="pâturages" /> et rapporte 3 par tour par bâtiment contrôlé</li>
-          <li>L'<AssetIcon asset="resources/gold.png" label="or" />, obtenable sur les <AssetIcon asset="buildings/null_mine.png" label="mines" /> et rapporte 1 par tour par bâtiment contrôlé</li>
+          <li>La <AssetIcon asset="resources:food" label="nourriture" />, obtenable sur les <AssetIcon asset="buildings:null_wheat" label="champs" /> et les <AssetIcon asset="buildings:null_pasture" label="pâturages" /> et rapporte 3 par tour par bâtiment contrôlé</li>
+          <li>L'<AssetIcon asset="resources:gold" label="or" />, obtenable sur les <AssetIcon asset="buildings:null_mine" label="mines" /> et rapporte 1 par tour par bâtiment contrôlé</li>
         </ul>
-        De plus, un château contrôlé produit 3 <AssetIcon asset="resources/food.png" label="nourriture" /> par tour.
+        De plus, un château contrôlé produit 3 <AssetIcon asset="resources:food" label="nourriture" /> par tour.
         Les ressources sont distribuées à la fin du tour du joueur, donc juste avant que l'adversaire ne joue.
         Toutes les troupes sauf les paysans ont un coût d'entretien : elles réduisent de 1 par troupe la nourriture gagnée en fin de tour.
       </TabsProseParagraph>
@@ -39,12 +39,12 @@
     
     <TabsProseSection title="Les Troupes" icon="i-mdi-sword">
       <TabsProseParagraph title="Château">
-        À chaque tour, un <AssetIcon asset="buildings/null_castle.png" label="château" /> peut créer autant de troupes que l'on veut tant que la case du château est libre.
-        Toutes les troupes sauf les paysans coûtent un <AssetIcon asset="resources/gold.png" label="or" /> en début de partie.
-        Les paysans coûtent un <AssetIcon asset="resources/food.png" label="nourriture" /> à la place d'un <AssetIcon asset="resources/gold.png" label="or" />.
+        À chaque tour, un <AssetIcon asset="buildings:null_castle" label="château" /> peut créer autant de troupes que l'on veut tant que la case du château est libre.
+        Toutes les troupes sauf les paysans coûtent un <AssetIcon asset="resources:gold" label="or" /> en début de partie.
+        Les paysans coûtent un <AssetIcon asset="resources:food" label="nourriture" /> à la place d'un <AssetIcon asset="resources:gold" label="or" />.
         Le coût de toutes les troupes augmente de +1 par troupe du même type déjà sur le terrain.<br>
-        <strong>Exemple :</strong> si deux cavaliers sont présents sur le terrain, le prochain cavalier coûtera 3 <AssetIcon asset="resources/gold.png" label="or" />.<br>
-        <strong>Remarque :</strong> chaque château produit trois <AssetIcon asset="resources/food.png" label="nourriture" /> par tour.
+        <strong>Exemple :</strong> si deux cavaliers sont présents sur le terrain, le prochain cavalier coûtera 3 <AssetIcon asset="resources:gold" label="or" />.<br>
+        <strong>Remarque :</strong> chaque château produit trois <AssetIcon asset="resources:food" label="nourriture" /> par tour.
       </TabsProseParagraph>
 
       <TabsProseParagraph title="Mur">
@@ -55,49 +55,49 @@
       </TabsProseParagraph>
 
       <TabsProseParagraph title="Soldat">
-        À chaque tour, un <AssetIcon asset="entities/null_melee.png" label="soldat" /> peut :
+        À chaque tour, un <AssetIcon asset="entities:null_melee" label="soldat" /> peut :
         <ul>
-          <li>Se déplacer d'une case contre une <AssetIcon asset="resources/food.png" label="nourriture" />.</li>
-          <li>Attaquer contre une <AssetIcon asset="resources/food.png" label="nourriture" /> et bénéficier d'un deuxième tour.</li>
+          <li>Se déplacer d'une case contre une <AssetIcon asset="resources:food" label="nourriture" />.</li>
+          <li>Attaquer contre une <AssetIcon asset="resources:food" label="nourriture" /> et bénéficier d'un deuxième tour.</li>
         </ul>
         <strong>Capacité :</strong> les soldats sont immunisés aux archers.
       </TabsProseParagraph>
 
       <TabsProseParagraph title="Archer">
-        À chaque tour, un <AssetIcon asset="entities/null_archer.png" label="archer" /> peut :
+        À chaque tour, un <AssetIcon asset="entities:null_archer" label="archer" /> peut :
         <ul>
-          <li>Se déplacer d'une case contre un <AssetIcon asset="resources/food.png" label="nourriture" />.</li>
-          <li>Attaquer jusqu'à deux cases de distance sans aller sur la case de la troupe ciblée contre un <AssetIcon asset="resources/food.png" label="nourriture" />.</li>
+          <li>Se déplacer d'une case contre un <AssetIcon asset="resources:food" label="nourriture" />.</li>
+          <li>Attaquer jusqu'à deux cases de distance sans aller sur la case de la troupe ciblée contre un <AssetIcon asset="resources:food" label="nourriture" />.</li>
         </ul>
         <strong>Remarque :</strong> les archers sont inoffensifs face aux soldats.
       </TabsProseParagraph>
 
       <TabsProseParagraph title="Cavalier">
-        À chaque tour, un <AssetIcon asset="entities/null_horseman.png" label="cavalier" /> peut :
+        À chaque tour, un <AssetIcon asset="entities:null_horseman" label="cavalier" /> peut :
         <ul>
-          <li>Se déplacer d'une case contre un <AssetIcon asset="resources/food.png" label="nourriture" /> et bénéficier d'un deuxième tour.</li>
-          <li>Attaquer contre un <AssetIcon asset="resources/food.png" label="nourriture" />.</li>
+          <li>Se déplacer d'une case contre un <AssetIcon asset="resources:food" label="nourriture" /> et bénéficier d'un deuxième tour.</li>
+          <li>Attaquer contre un <AssetIcon asset="resources:food" label="nourriture" />.</li>
         </ul>
       </TabsProseParagraph>
 
       <TabsProseParagraph title="Bâtisseur">
-        À chaque tour, un <AssetIcon asset="entities/null_builder.png" label="bâtisseur" /> peut :
+        À chaque tour, un <AssetIcon asset="entities:null_builder" label="bâtisseur" /> peut :
         <ul>
-          <li>Construire ou détruire jusqu'à 99 bâtiments contre un <AssetIcon asset="resources/food.png" label="nourriture" /> et/ou construire un <AssetIcon asset="buildings/null_wall.png" label="mur" /> à chaque fois, puis se déplacer d'une case contre un <AssetIcon asset="resources/food.png" label="nourriture" />.</li>
-          <li>Se déplacer d'une case contre un <AssetIcon asset="resources/food.png" label="nourriture" />.</li>
+          <li>Construire ou détruire jusqu'à 99 bâtiments contre un <AssetIcon asset="resources:food" label="nourriture" /> et/ou construire un <AssetIcon asset="buildings:null_wall" label="mur" /> à chaque fois, puis se déplacer d'une case contre un <AssetIcon asset="resources:food" label="nourriture" />.</li>
+          <li>Se déplacer d'une case contre un <AssetIcon asset="resources:food" label="nourriture" />.</li>
         </ul>
         <strong>Capacité :</strong> si un bâtisseur appartient à un joueur n'ayant plus de château, sa prochaine construction sera un château et non un mur.<br>
         <strong>Remarque :</strong> les bâtisseurs peuvent détruire les obstacles naturels et les bâtiments de ressources ainsi que les châteaux ennemis.
       </TabsProseParagraph>
 
       <TabsProseParagraph title="Paysan">
-        À chaque tour, un <AssetIcon asset="entities/null_farmer.png" label="paysan" /> peut :
+        À chaque tour, un <AssetIcon asset="entities:null_farmer" label="paysan" /> peut :
         <ul>
-          <li>Se déplacer d'une case contre un <AssetIcon asset="resources/food.png" label="nourriture" />.</li>
-          <li>Attaquer contre un <AssetIcon asset="resources/food.png" label="nourriture" />.</li>
+          <li>Se déplacer d'une case contre un <AssetIcon asset="resources:food" label="nourriture" />.</li>
+          <li>Attaquer contre un <AssetIcon asset="resources:food" label="nourriture" />.</li>
         </ul>
         <strong>Capacité :</strong> les paysans ne coûtent rien à entretenir.<br>
-        <strong>Capacité :</strong> les paysants sont recrutés à l'aide de <AssetIcon asset="resources/food.png" label="nourriture" /> au lieu de l'<AssetIcon asset="resources/gold.png" label="or" />.
+        <strong>Capacité :</strong> les paysants sont recrutés à l'aide de <AssetIcon asset="resources:food" label="nourriture" /> au lieu de l'<AssetIcon asset="resources:gold" label="or" />.
       </TabsProseParagraph>
     </TabsProseSection>
 
@@ -107,7 +107,7 @@
       </TabsProseParagraph>
 
       <TabsProseParagraph title="Obstacles Naturels">
-        Les <AssetIcon asset="buildings/null_mountain.png" label="montagnes" /> et <AssetIcon asset="buildings/null_lake.png" label="lacs" /> empêchent le déplacement.<br>
+        Les <AssetIcon asset="buildings:null_mountain" label="montagnes" /> et <AssetIcon asset="buildings:null_lake" label="lacs" /> empêchent le déplacement.<br>
         <strong>Remarque:</strong> ils peuvent être détruits par les bâtisseurs.
       </TabsProseParagraph>
     </TabsProseSection>
